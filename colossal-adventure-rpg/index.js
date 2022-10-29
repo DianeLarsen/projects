6496f069c37f6693196d6b33532af4958079bc7d
Diane Larsen <diane.larsen@outlook.com>
2022-10-29 13:29:55 -0700

    final product

--- colossal-adventure-rpg/index.js
+++ colossal-adventure-rpg/index.js
@@ -1,47 +1,32 @@
 const readline = require("readline-sync");
 const chalk = require('chalk');
-let itemsInventory = [
+let itemsInventory = [ //current inventory
     {type: "sword",color: (chalk.whiteBright("White")), typeBuff: "damange dealt", buff: 0}, 
     {type: "armor", color: (chalk.whiteBright("White")), typeBuff: "health points",buff: 0}, 
     {type: "shield", color: (chalk.whiteBright("White")), typeBuff: "damage recieved", buff: 0} 
 ];
-const maxHP = 40;
-let enemyDebuff = 1;
-let enemyHP = 0;
-let damageDeltBuff = (itemsInventory[0].buff + 1)
-let damagerecieved = itemsInventory[2].buff
-
-let hp = Math.floor(maxHP*((itemsInventory[1].buff)+1)); 
-
-
-
+const maxHP = 40; // basic Health points
+let enemyHP = 0; // declaring enemyHPs
+let damageDeltBuff = (itemsInventory[0].buff + 1) // adds to the damage delt to the enemy
+let damagerecieved = itemsInventory[2].buff // reduces damage dealt from enemy
+let hp = Math.floor(maxHP*((itemsInventory[1].buff)+1)); // health points with buff added
 const damageTaken = chalk.bold.redBright;
 const hpPoints = chalk.bold.cyanBright;
 const success = chalk.greenBright;
 const warning = chalk.hex('#FFA500'); 
 const userInput = chalk.yellowBright;
-
-
 const items = [{type: "sword", color: (chalk.rgb(0,0,255)("Blue")), typeBuff: "damange dealt", buff: .1}, {type: "sword", color: (chalk.rgb(255,0,255)("Pink")), typeBuff: "damange dealt", buff: .2},
  {type: "sword", color: (chalk.rgb(255,215,0)("Gold")), typeBuff: "damange dealt", buff: .3}, {type: "armor", color: (chalk.rgb(0,0,255)("Blue")), typeBuff: "health points", buff: .1}, {type: "armor", color: (chalk.rgb(255,0,255)("Pink")), typeBuff: "health points", buff: .2},
  {type: "armor", color: (chalk.rgb(255,215,0)("Gold")), typeBuff: "health points", buff: .3}, {type: "shield", color: (chalk.rgb(0,0,255)("Blue")), typeBuff: "damage recieved", buff: .1}, {type: "shield", color: (chalk.rgb(255,0,255)("Pink")), typeBuff: "damage recieved", buff: .2}, {type: "shield", color: (chalk.rgb(255,215,0)("Gold")), typeBuff: "damage recieved", buff: .3}];
- 
-
 const enemies = [
     { type: "skelleton", hp: 50},
     { type: "goblin", hp: 30}, 
     { type: "troll", hp: 20}
 ];
-
 const name = readline.question("Welcome! By what name shall I call you? ");
-
-//console.log("function " + findAnItem());
-
 console.log(success("Hello, " +(userInput(name))+ " the adventurer!"));
 console.log(player())
 console.log("You're on a dimly lit coblestone path in a forest.  You have no recollection about how you arrived here, but you feel the urge to find a way out.");
-
-walkStart();
 
 function walkStart(){
     let choices = readline.question( 
@@ -76,7 +61,7 @@ function walkStart(){
         walkStart()
     }
 };
-    
+  
 function findAnItem(){
 
     let pickedupItem = items[Math.floor(Math.random() * items.length)];
@@ -103,7 +88,7 @@ function findAnItem(){
     console.log(inventory());  // name prettier
     walkStart();
 };
-// make this pretty
+
 function inventory() { 
     //console.log((chalk.black.bgYellowBright(itemsInventory[0].type +" " +itemsInventory[1].type +" "+ itemsInventory[2].type)))
     console.log("The items in your inventory: "
@@ -114,27 +99,46 @@ function inventory() {
 
 }
 function keyPress(){
-    readline.keyInPause();
+    readline.keyInPause("Hit <SPACE> button to continue...");
 
 }
   
 function player(){
-    console.log("Hello adventurer "+(userInput(name))+ ".  You have "+(chalk.cyanBright(hp))+" health points.")
+    console.log("Hello adventurer "+(userInput(name))+ ".  You have "+(hpPoints(hp))+" health points.")
     console.log(inventory())
     walkStart()
 }
 
  function walk(){
-    console.log("You are on the bank of a fast flowing river with a bridge.");
-    // console.log("You are on the bank of a fast flowing river with a bridge.");
-    // console.log("You are on the bank of a fast flowing river with a bridge.");
-    // console.log("You are on the bank of a fast flowing river with a bridge.");
-    // console.log("You are on the bank of a fast flowing river with a bridge.");
+    var randomQuote = Math.floor(Math.random()*11)
+    if (randomQuote == 0){
+        console.log("You walk along the bank of a fast flowing river with a bridge.");
+    } else if (randomQuote == 1){
+        console.log("You walk along a tree lined forest.");
+    } else if (randomQuote == 2){
+        console.log("You walk at the edge of a cliff with a beatiful view of the ocean.");
+    } else if (randomQuote == 3){
+        console.log("You walk along a trail through tall grass.");
+    } else if (randomQuote == 4){
+        console.log("You walk across a stream.");
+    } else if (randomQuote == 5){
+        console.log("You walk by a temple, you are not sure who it is devoted to.");
+    } else if (randomQuote == 6){
+        console.log("You walk along a beach with the sound of waves crashing nearby.");
+    } else if (randomQuote == 7){
+        console.log("You walk to the end of a pier, there are no boats nearby.");
+    } else if (randomQuote == 8){
+        console.log("You are on the bank of a fast flowing river with a bridge.");
+    } else if (randomQuote == 9){
+        console.log("You walk along a trail through tall grass.");
+    } else if (randomQuote == 10){
+        console.log("You walk at the edge of a cliff with a beatiful view of the ocean.");
+    }
+    
     walkStart()
- }
+}
 
 function fight(){
-     //const winner = selectWinner()
      let activeEnemy = enemySelect()
      enemyHP = activeEnemy["hp"]
      let choices = readline.question( 
@@ -147,7 +151,7 @@ function fight(){
       if (activeEnemy["type:"] == "skelleton"){
         
         let choices = readline.question( 
-            (("You have encountered a"+(chalk.redBright(" skelleton"))+" with Health points of "+(chalk.redBright(enemyHP))+". Your Health points are a"+(chalk.cyanBright(hp))+", what do you do?"))
+            (("You have encountered a"+(chalk.redBright(" skelleton"))+" with Health points of "+(chalk.redBright(enemyHP))+". Your Health points are a"+(hpPoints(hp))+", what do you do?"))
             +"\npress "+(damageTaken('a')) +" to attack"
             +"\npress "+(damageTaken('r')) +" to attempt to run away"  
                     +"\nEnter choice: " );
@@ -161,7 +165,7 @@ function fight(){
           }else if (activeEnemy["type:"] == "goblin"){
             
               let choices = readline.question( 
-                (("You have encountered a"+(chalk.redBright(" goblin"))+" with Health points of "+(chalk.redBright(enemyHP))+". Your Health points are "+(chalk.cyanBright(hp))+", what do you do?"))
+                (("You have encountered a"+(chalk.redBright(" goblin"))+" with Health points of "+(chalk.redBright(enemyHP))+". Your Health points are "+(hpPoints(hp))+", what do you do?"))
                 +"\npress "+(damageTaken('a')) +" to attack"
                 +"\npress "+(damageTaken('r')) +" to attempt to run away" 
                         +"\nEnter choice: " );
@@ -176,7 +180,7 @@ function fight(){
           }else{
             
             let choices = readline.question( 
-                (("You have encountered a"+(chalk.redBright(" troll"))+" with Health points of "+(chalk.redBright(enemyHP))+". Your Health points are "+(chalk.cyanBright(hp))+", what do you do?"))
+                (("You have encountered a"+(chalk.redBright(" troll"))+" with Health points of "+(chalk.redBright(enemyHP))+". Your Health points are "+(hpPoints(hp))+", what do you do?"))
                 +"\npress "+(damageTaken('a')) +" to attack"
                 +"\npress "+(damageTaken('r')) +" to attempt to run away"  
                         +"\nEnter choice: " );
@@ -208,7 +212,7 @@ function runAway(){
         hp = (hp - damage) 
         console.log("You have failed to run away and recieved "+ (chalk.redBright(damage)) + " damage, you are still in a fight!")
         let choices = readline.question( 
-            ("Your life is at "+(chalk.cyanBright(hp))+", what do you do?")
+            ("Your life is at "+(hpPoints(hp))+", what do you do?")
             +"\npress "+(damageTaken('f')) +" to fight"
             +"\npress "+(damageTaken('r')) +" to attempt to run away"  
                     +"\nEnter choice: " );
@@ -228,14 +232,11 @@ function enemySelect(){
 }
 
 function enemyEncounter(){
-   
-    //console.log(chalk.redBright("fight fight fight"))         
-
-    console.log(chalk.redBright("Your enemy runs at you with a scary looking weapon"))
-while ((hp > 0) && (enemyHP > 0)){
-    // attack from enemy
-    let attack = Math.floor(Math.random()*11);
-    attack = attack-Math.floor(attack*damagerecieved); // attack from enemy
+   console.log(chalk.redBright("Your enemy runs at you with a scary looking weapon"))
+    while ((hp > 0) && (enemyHP > 0)){
+        // attack from enemy
+        let attack = Math.floor(Math.random()*11);
+        attack = attack-Math.floor(attack*damagerecieved); 
         if (attack > 9){
             attack = Math.floor(attack*1.3)
             console.log("Enemy has critiacally hit you for "+(attack));
@@ -246,60 +247,36 @@ while ((hp > 0) && (enemyHP > 0)){
             hp = Math.max(0, (hp - attack))   
         }
   
-    // hit from player
+        // hit from player
     
-    let hit = ((Math.floor(Math.random()*11))*damageDeltBuff); // attack from player, needs buff added
-    
-  
-    if (hit == 0) {
-        console.log("The enemy dodged your attack");
-    } else if (attack > 9){
-        hit = Math.floor(hit*1.1);
-        console.log("You have critiacally hit enemy for "+(hit));
-    } else {
-        console.log("You hit the enemy for " + (damageTaken(attack)) + " health!");
-        enemyHP = Math.floor(Math.max(0, (enemyHP - attack)))  
-    };
-    console.log(("Enemies Health points are "+(chalk.redBright(enemyHP))+". Your Health points are "+(chalk.cyanBright(hp))))
+        let hit = ((Math.floor(Math.random()*11))*damageDeltBuff); // attack from player, needs buff added
+        if (hit == 0) {
+            console.log("The enemy dodged your attack");
+        } else if (attack > 9){
+            hit = Math.floor(hit*1.1);
+            console.log("You have critiacally hit enemy for "+(hit));
+        } else {
+            console.log("You hit the enemy for " + (damageTaken(attack)) + " health!");
+            enemyHP = Math.floor(Math.max(0, (enemyHP - attack)))  
+        };
+    console.log(("Enemies Health points are "+(chalk.redBright(enemyHP))+". Your Health points are "+(hpPoints(hp))))
     keyPress();
 }
-if (hp == 0){
-    console.log((warning("You cannot say “Not Today” forever."))+(chalk.redBright("\nYou have died, Game OVER!")))
-    process.exit()
+    // dealth messages
+    if (hp == 0){
+        console.log((warning("You cannot say “Not Today” forever."))+(chalk.redBright("\nYou have died, Game OVER!")))
+        process.exit()
+    }
+    if (enemyHP == 0){
+        console.log(warning("'Don't think you've won this. This isn't over yet!'"))
+        console.log(success("The enemy has been killed, it has dropped an item!"))
+        findAnItem()
+    }
 }
-if (enemyHP == 0){
-    console.log(warning("'Don't think you've won this. This isn't over yet!'"))
-    console.log(success("The enemy has been killed, it has dropped an item!"))
-    findAnItem()
-}
-}
-
-//You are inside a small yet sacrosanct shrine. A sense of deep respectfulness fills this modest room. The way out, into a pine forest, is to the northwest. It is obvious that the shrine was meant to be used for quiet meditation, like similar chambers.
-
-//*kill skeleton
-
-// The viciousness of a whack by the skeleton sends you sideways.
-// Dazedly you pull through, and press forward into the contest.
-// Your mis-timed return blow at the skeleton is effortlessly shrugged off.
-// You easily evade a poor swing from the skeleton.
-// You bash the skeleton with a punishing forehand!
-// You comfortably shrug off a feeble thump by the skeleton.
-// You wallop the skeleton with a crushing whack!
-// Your last swing took the life of the skeleton!
-// You are victorious - this time...
-
-//You are on the bank of a fast flowing river with pasture to the north and forest to the south.
-
-// walk => 30% chance enemyEncounter => fight/runAway => 
-// fight => enemyAttack => kill/bekilled =>
-// runAway => 50% change of success and random% loss of life (hit from behind) => walk
-// kill => inventory => healling => 10% Bonus to HP for 2 turns => skill points
-
-// stat increases/decreases reduce/increase chance encounters 
 
 
-// find new item (Sword, Armor, Potion)
-// display new item
-// ask if want to replace old item with new item in inventory y/n question
-// if yes replace item "new item has replaced the (whatever) in your inventory"
-// if no do nothing "item was dropped and can no longer be picked up"
+
+
+
+
+
