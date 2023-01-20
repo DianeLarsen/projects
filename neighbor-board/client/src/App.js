23f74010d91e9827d26466c0e82472e7a37c7b70
diane larsen <diane.larsen@outlook.com>
2023-01-20 13:15:13 -0800

    added groundwork for server

--- neighbor-board/client/src/App.js
+++ neighbor-board/client/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header></header>
+      <footer></footer>
     </div>
   );
 }
---
+++ neighbor-board/client/src/components/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+
+export default function Header() {
+  return (
+    <div className="header-wrapper-outer" id="grid-container">
+      <a className="logo" href="/#">Placeholder for logo/pic
+        {/* <img className="logo" src={bookLogo} alt="We Be Bookn" /> */}
+      </a>
+      <div className="navigation-wrapper">
+        <ul className="nav" id="grid-container-nav">
+          <li>
+            <a className="navList" href="#news">
+              HOME
+            </a>
+          </li>
+          <li>
+            <a className="navList" href="#store">
+              Neighbor Info
+            </a>
+          </li>
+          <li>
+            <a className="navList" href="#contact">
+              Posts
+            </a>
+          </li>
+          <li>
+            <a className="navList" href="#about">
+              SEARCH
+            </a>
+          </li>
+          <li>
+            
+            <svg data-v-43540933="" width="24px" height="24px" xmlns="http://www.w3.org/2000/svg" role="img" className="svg-user">
+
+            <use href="#user-icon" ></use>
+            </svg>
+            
+          </li>
+   
+        </ul>
+        
+      </div>
+    </div>
+  );
+}
