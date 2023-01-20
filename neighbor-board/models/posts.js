358d4833b172df9da6c8bfd78a4a23e35b6c090a
diane larsen <diane.larsen@outlook.com>
2023-01-20 13:04:07 -0800

    added groundwork for server

---
+++ neighbor-board/models/posts.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+const postSchema = new Schema({
+    lot: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("Posts", postSchema)
---
+++ neighbor-board/routes/lotRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const lotRouter = express.Router();
+const Lots = require("../models/lots.js");
+
+
+
+
+
+
+
+module.exports = lotRouter;
