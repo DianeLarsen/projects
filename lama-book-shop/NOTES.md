3da0797c62cc4829e7d95b4dd57bc2efa5a3d466
diane larsen <diane.larsen@outlook.com>
2023-02-16 13:08:24 -0800

    leaning sql

---
+++ lama-book-shop/NOTES.md
@@ -0,0 +1,18 @@
+downloaded MySQL from dev.MySQL.com
+EllaBella0678*
+
+
+create client and backend folders
+cd into backend
+
+npm init -y
+npm i express mysql nodemon
+
+import express from "express"
+
+it requires to add 
+  "type": "module",
+  into package.json under main
+
+
+  https://www.youtube.com/watch?v=fPuLnzSjPLE&list=PLj-4DlPRT48nfYgDK00oTjlDF4O0ZZyG8&index=38
---
+++ lama-book-shop/backend/server.js
@@ -0,0 +1,86 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  port: 3306,
+  password: "password",
+  database: "test",
+  
+});
+
+// db.connect(function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Connected!");
+//   }
+// });
+
+app.get("/", (req, res) => {
+  res.json("hello this is the backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM test.books";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+// app.post("/books", (req, res) => {
+//   const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+
+//   const values = [
+//     req.body.title,
+//     req.body.desc,
+//     req.body.price,
+//     req.body.cover,
+//   ];
+
+//   db.query(q, [values], (err, data) => {
+//     if (err) return res.send(err);
+//     return res.json(data);
+//   });
+// });
+
+// app.delete("/books/:id", (req, res) => {
+//   const bookId = req.params.id;
+//   const q = " DELETE FROM books WHERE id = ? ";
+
+//   db.query(q, [bookId], (err, data) => {
+//     if (err) return res.send(err);
+//     return res.json(data);
+//   });
+// });
+
+// app.put("/books/:id", (req, res) => {
+//   const bookId = req.params.id;
+//   const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+
+//   const values = [
+//     req.body.title,
+//     req.body.desc,
+//     req.body.price,
+//     req.body.cover,
+//   ];
+
+//   db.query(q, [...values,bookId], (err, data) => {
+//     if (err) return res.send(err);
+//     return res.json(data);
+//   });
+// });
+
+app.listen(9000, () => {
+  console.log("Connected to backend.");
+});
